1f12979ecfef08990762d9bdd0ddc8166a010981
Muhamad Ridwan <ridwanskaterocks@gmail.com>
2022-12-22 09:44:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+var PouchDB = require('pouchdb');
+var express = require('express');
+var app = express();
+
+app.use('/db', require('express-pouchdb')(PouchDB));
+
+app.listen(3000);
